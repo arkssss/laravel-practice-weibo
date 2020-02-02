fd409ec5d337bedf538546d0658f6cd48d13714f
arkssss <522500442@qq.com>
2020-02-02 08:26:04 +0000

    粉丝表

--- app/Models/User.php
+++ app/Models/User.php
@@ -46,6 +46,17 @@ class User extends Authenticatable
         return "http://www.gravatar.com/avatar/$hash?s=$size";
     }
 
+    public function followers(){
+
+        return $this->belongsToMany(User::class, 'follows', 'follower_id', 'user_id');
+
+    }
+
+    public function followings(){
+
+        return $this->belongsToMany(User::class, 'follows', 'user_id', 'follower_id');
+
+    }
 
     public function blogs(){
 
